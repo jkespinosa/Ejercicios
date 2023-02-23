c6b38a9b7665496e9f425194ce169fd83d05fa23
jkespinosa <jessy.klixto@gmail.com>
2023-02-22 18:57:54 -0600

    Initial commit

---
+++ EjerciciosSesion1/EjerciciosSesion1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Jessica Espinosa");
+Console.WriteLine("");
+Console.WriteLine("");
+
+Console.WriteLine("Ingrese una hora");
+var Hour = Console.ReadLine();
+Console.WriteLine("La hora ingresada manualmente es: {0}", Hour);
+Console.WriteLine("");
+Console.WriteLine("");
+
+var SystemHour = DateTime.Now.Hour;
+var SystemMinute = DateTime.Now.Minute;
+
+Console.WriteLine("La hora del sistema es: {0}:{1}", SystemHour, SystemMinute);
